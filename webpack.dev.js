2b98a5496067166732312b842f6bb741d6a15648
Rahul Mohuture <97466426+6rahul9@users.noreply.github.com>
2024-09-26 11:39:59 +0000

    added all new packages

--- webpack.dev.js
+++ webpack.dev.js
@@ -4,15 +4,42 @@ const PORT = process.env.PORT || 3000
 const path = require('path')
 const buildDir = path.join(__dirname, 'build')
 
-module.exports = w.merge(common, {
+// module.exports = w.merge(common, {
+//   mode: 'development',
+//   devtool: 'inline-source-map',
+//   devServer: {
+//     contentBase: buildDir,
+//     compress: true,
+//     port: PORT,
+//     watchOptions: {
+//       poll: true,
+//     },
+//   },
+// })
+
+module.exports = {
   mode: 'development',
-  devtool: 'inline-source-map',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
   devServer: {
-    contentBase: buildDir,
+    static: {
+      directory: path.join(__dirname, 'public'), // Serve static files from the 'public' directory
+    },
     compress: true,
-    port: PORT,
-    watchOptions: {
-      poll: true,
+    port: 3000,
+    open: true,
+    hot: true,
+    client: {
+      overlay: true, // Show errors in the browser overlay
+    },
+    watchFiles: {
+      paths: ['src/**/*.js', 'public/**/*'], // Watch specific files or directories for changes
+      options: {
+        usePolling: true, // Adjust this option based on your needs
+      },
     },
   },
-})
+};
