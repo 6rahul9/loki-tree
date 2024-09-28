191d5832fd92bc948a5c01d6f9c7a517081ab8f7
Rahul Mohuture <97466426+6rahul9@users.noreply.github.com>
2024-09-28 06:34:23 +0000

    adding the files

--- webpack.common.js
+++ webpack.common.js
@@ -1,29 +1,151 @@
-const path = require('path')
-const HandlebarsPlugin = require('handlebars-webpack-plugin')
-const CopyWebpackPlugin = require('copy-webpack-plugin')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const postcssPresetEnv = require('postcss-preset-env')
-const ImageminPlugin = require('imagemin-webpack-plugin').default
-const cssnano = require('cssnano')
+// const path = require('path')
+// const HandlebarsPlugin = require('handlebars-webpack-plugin')
+// const CopyWebpackPlugin = require('copy-webpack-plugin')
+// const HtmlWebpackPlugin = require('html-webpack-plugin')
+// const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+// const postcssPresetEnv = require('postcss-preset-env')
+// const ImageminPlugin = require('imagemin-webpack-plugin').default
+// const cssnano = require('cssnano')
 
-const { registerHandlersHelpers } = require('./webpack.helpers.js')
+// const { registerHandlersHelpers } = require('./webpack.helpers.js')
 
-const mode = process.env.NODE_ENV || 'production'
+// const mode = process.env.NODE_ENV || 'production'
 
-const sourceDir = path.join(__dirname, 'src')
-const templateDir = path.join(__dirname, 'generated')
-const buildDir = path.join(__dirname, 'build')
+// const sourceDir = path.join(__dirname, 'src')
+// const templateDir = path.join(__dirname, 'generated')
+// const buildDir = path.join(__dirname, 'build')
 
-const isProd = mode === 'production'
-const prodPlugins = [new ImageminPlugin({ test: /\.(jpeg|png|gif|svg)$/i })]
+// const isProd = mode === 'production'
+// const prodPlugins = [new ImageminPlugin({ test: /\.(jpeg|png|gif|svg)$/i })]
+
+// module.exports = {
+//   mode,
+//   devtool: 'source-map',
+//   entry: path.join(sourceDir, 'entry.js'),
+//   output: {
+//     filename: isProd ? 'bundle.[chunkhash].js' : './bundle.js',
+//     path: buildDir,
+//     publicPath: '/',
+//   },
+//   resolve: {
+//     alias: {
+//       '~constants': `${sourceDir}/js/constants`,
+//       '~managers': `${sourceDir}/js/managers`,
+//       '~utils': `${sourceDir}/js/utils`,
+//     },
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: /\.js$/,
+//         exclude: /node_modules/,
+//         use: {
+//           loader: 'babel-loader',
+//         },
+//       },
+//       {
+//         test: /\.scss$/,
+//         use: [
+//           MiniCssExtractPlugin.loader,
+//           { loader: 'css-loader', options: { importLoaders: 1 } },
+//           isProd
+//             ? {
+//               loader: 'postcss-loader',
+//               options: {
+//                 ident: 'postcss',
+//                 plugins: () => [postcssPresetEnv(), cssnano()],
+//               },
+//             }
+//             : null,
+//           'sass-loader',
+//         ].filter(Boolean),
+//       },
+//       {
+//         test: /\.(eot|ttf|woff|woff2)$/,
+//         use: [
+//           {
+//             loader: 'file-loader',
+//             options: { name: 'fonts/[name].[ext]' },
+//           },
+//         ],
+//       },
+//       {
+//         test: /\.(glsl|vs|fs|vert|frag)$/,
+//         use: ['webpack-glsl-loader'],
+//       },
+//       {
+//         test: /\.(jpe?g|png|gif|svg)$/i,
+//         use: [
+//           {
+//             loader: 'ignore-loader',
+//           },
+//         ],
+//       },
+//     ],
+//   },
+//   plugins: [
+//     new HtmlWebpackPlugin({
+//       template: path.join(sourceDir, 'views', 'layout', 'template.hbs'),
+//       filename: path.join(templateDir, 'template.hbs'),
+//       inject: false,
+//     }),
+//     new HandlebarsPlugin({
+//       htmlWebpackPlugin: {
+//         enabled: true,
+//         prefix: 'html',
+//       },
+//       entry: path.join(sourceDir, 'views', '*.hbs'),
+//       output: name => {
+//         const page = name !== 'index' ? name : ''
+//         return path.join(buildDir, page, 'index.html')
+//       },
+//       data: path.join(sourceDir, 'data', '*.json'),
+//       partials: [path.join(templateDir, 'template.hbs'), path.join(sourceDir, 'views', '*', '*.hbs')],
+//       onBeforeSetup: Handlebars => {
+//         return registerHandlersHelpers(Handlebars)
+//       },
+//     }),
+//     new CopyWebpackPlugin([
+//       {
+//         from: path.join(sourceDir, 'img'),
+//         to: 'img',
+//       },
+//     ]),
+//     new MiniCssExtractPlugin({
+//       filename: isProd ? '[name].[chunkhash].css' : '[name].css',
+//       chunkFilename: '[id].css',
+//       fallback: 'style-loader',
+//       use: [{ loader: 'css-loader', options: { minimize: isProd } }],
+//     }),
+//   ].concat(isProd ? prodPlugins : [])
+// }
+
+const path = require('path');
+const HandlebarsPlugin = require('handlebars-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const postcssPresetEnv = require('postcss-preset-env');
+const ImageminPlugin = require('imagemin-webpack-plugin').default;
+const cssnano = require('cssnano');
+
+const { registerHandlersHelpers } = require('./webpack.helpers.js');
+
+const mode = process.env.NODE_ENV || 'production';
+
+const sourceDir = path.join(__dirname, 'src');
+const templateDir = path.join(__dirname, 'generated');
+const buildDir = path.join(__dirname, 'build');
+
+const isProd = mode === 'production';
+const prodPlugins = [new ImageminPlugin({ test: /\.(jpeg|png|gif|svg)$/i })];
 
 module.exports = {
   mode,
   devtool: 'source-map',
   entry: path.join(sourceDir, 'entry.js'),
   output: {
-    filename: isProd ? 'bundle.[chunkhash].js' : './bundle.js',
+    filename: isProd ? 'bundle.[contenthash].js' : 'bundle.js', // Use [contenthash] for better cache management
     path: buildDir,
     publicPath: '/',
   },
@@ -48,17 +170,16 @@ module.exports = {
         use: [
           MiniCssExtractPlugin.loader,
           { loader: 'css-loader', options: { importLoaders: 1 } },
-          isProd
-            ? {
-              loader: 'postcss-loader',
-              options: {
-                ident: 'postcss',
-                plugins: () => [postcssPresetEnv(), cssnano()],
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [postcssPresetEnv(), isProd ? cssnano() : null].filter(Boolean),
               },
-            }
-            : null,
+            },
+          },
           'sass-loader',
-        ].filter(Boolean),
+        ],
       },
       {
         test: /\.(eot|ttf|woff|woff2)$/,
@@ -75,19 +196,19 @@ module.exports = {
       },
       {
         test: /\.(jpe?g|png|gif|svg)$/i,
-        use: [
-          {
-            loader: 'ignore-loader',
-          },
-        ],
+        type: 'asset/resource',
+        generator: {
+          filename: 'images/[name][ext]', // Output images in a separate folder
+        },
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: path.join(sourceDir, 'views', 'layout', 'template.hbs'),
-      filename: path.join(templateDir, 'template.hbs'),
-      inject: false,
+      filename: 'index.html', // Change the filename to output directly in the build directory
+      inject: 'body', // Ensures scripts are injected at the end of the body
+      minify: isProd, // Minifies HTML in production mode
     }),
     new HandlebarsPlugin({
       htmlWebpackPlugin: {
@@ -96,26 +217,27 @@ module.exports = {
       },
       entry: path.join(sourceDir, 'views', '*.hbs'),
       output: name => {
-        const page = name !== 'index' ? name : ''
-        return path.join(buildDir, page, 'index.html')
+        const page = name !== 'index' ? name : '';
+        return path.join(buildDir, page, 'index.html');
       },
       data: path.join(sourceDir, 'data', '*.json'),
       partials: [path.join(templateDir, 'template.hbs'), path.join(sourceDir, 'views', '*', '*.hbs')],
       onBeforeSetup: Handlebars => {
-        return registerHandlersHelpers(Handlebars)
+        return registerHandlersHelpers(Handlebars);
       },
     }),
-    new CopyWebpackPlugin([
-      {
-        from: path.join(sourceDir, 'img'),
-        to: 'img',
-      },
-    ]),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.join(sourceDir, 'img'),
+          to: 'img',
+        },
+      ],
+    }),
     new MiniCssExtractPlugin({
-      filename: isProd ? '[name].[chunkhash].css' : '[name].css',
+      filename: isProd ? '[name].[contenthash].css' : '[name].css',
       chunkFilename: '[id].css',
-      fallback: 'style-loader',
-      use: [{ loader: 'css-loader', options: { minimize: isProd } }],
     }),
-  ].concat(isProd ? prodPlugins : [])
-}
+    ...(isProd ? prodPlugins : []), // Only add production-specific plugins when needed
+  ],
+};
